7ae790bd838a00da3044c0f0cb353c4d2cf40066
Mikko Esko <mikko.esko@student.tut.fi>
2018-12-02 04:41:25 +0200

    Renamed a variable

--- Day01.js
+++ Day01.js
@@ -3,6 +3,6 @@ eval(document.body.innerText)
 
 // Part 2:
 // Note that this would NOT work if the input could contain 0
-for(let [sums, s, input] = [{0:1}, 0, document.body.innerText.split("\n").map(Number)];;) {
-    s = input.reduce((a, b) => { if (sums[a+b]) throw a+b; sums[a] = 1; return a+b }, s)
+for(let [seen, s, input] = [{0:1}, 0, document.body.innerText.split("\n").map(Number)];;) {
+    s = input.reduce((a, b) => { if (seen[a+b]) throw a+b; seen[a] = 1; return a+b }, s)
 }
